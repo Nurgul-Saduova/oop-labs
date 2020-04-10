bc883797a6560d1fe206bc547681e0f69979eeae
Nurgul-Saduova <61941186+Nurgul-Saduova@users.noreply.github.com>
2020-04-10 21:52:33 +0600

    Update AddLesson.kt

--- src/main/kotlin/component/AddLesson.kt
+++ src/main/kotlin/component/AddLesson.kt
@@ -1,20 +1,11 @@
 package component
 
-import react.*
-import react.dom.*
-import kotlinx.html.js.*
-import data.Lesson
-import data.lessonsList
 import kotlinx.html.InputType
-import kotlinx.html.id
 import kotlinx.html.js.onClickFunction
-import kotlinx.html.select
-import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.Event
 import react.RBuilder
 import react.RProps
 import react.child
-import react.dom.button
 import react.dom.h3
 import react.dom.input
 import react.functionalComponent
