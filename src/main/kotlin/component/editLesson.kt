dde9f8cd21f780381ccb01828ec004c7a93be3d4
Nurgul-Saduova <61941186+Nurgul-Saduova@users.noreply.github.com>
2020-04-27 15:17:23 +0600

    Update editLesson.kt

--- src/main/kotlin/component/editLesson.kt
+++ src/main/kotlin/component/editLesson.kt
@@ -1,30 +1,22 @@
 package component
 
-import data.Lesson
 import kotlinx.html.InputType
 import kotlinx.html.id
-import kotlinx.html.js.onClickFunction
-import org.w3c.dom.HTMLInputElement
-import org.w3c.dom.events.Event
 import react.*
-import react.dom.h3
 import react.dom.input
-import react.dom.li
-import react.dom.span
+
 
 interface EditlessonProps : RProps {
-    var lesson: Lesson
 }
 
 val feditLesson =
     functionalComponent<EditlessonProps> { props ->
-        li{
-            +props.lesson.name
+        input(type = InputType.text)  {
+            attrs.placeholder = "Enter lesson title"
+            attrs.id ="LessonAdd"
         }
     }
 
 fun RBuilder.editlesson(
-    lesson: Lesson
 ) = child(feditLesson) {
-    attrs.lesson = lesson
 }
